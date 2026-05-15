d93a998f4d91b686c4c59cba4558fdf18f4c0afb
samuel-sampan <business.samuelolivares@gmail.com>
2026-05-15 12:34:50 +0200

    Create inl_grupp15.R with assignment structure
    Initial implementation of assignment functions and setup.

---
+++ inl_grupp15.R
@@ -0,0 +1,77 @@
+#### Programmering i R ####
+#### Datorlaboration  [nummer] ####
+
+# spara denna fil enligt:
+# inl3_grupp01.R om ni tillhör grupp 01 och det är inl 3.
+
+
+# Skiv era namn här
+Namn <- c("namn1", "namn2")
+
+
+# Skiv era liu ID här
+LiuId  <- c("liuId1", "liuId2")
+
+# På del 2 i kursen, när ni arbetar i par, ange även grupptillhörighet enligt: 
+# Grupp<-"grupp01", om ni tillhör grupp01
+Grupp<-""
+
+
+
+# Uppgift 1
+#' Title
+#'
+#' @description
+#'
+#' @param N 
+#' @param K 
+#'
+#' @return
+sum_of_dice <- function(N, K){
+  # Skriv din funktion här
+  # Se datorövningarna för information om denna funktion
+}
+
+#' Title
+#'
+#' @description
+#'
+#' @param K 
+#' @param lambda 
+#' @param my_seed 
+#'
+#' @return
+sum_of_random_dice <- function(K, lambda, my_seed){
+  # Skriv din funktion här
+}
+
+
+# Uppgift 2 ----
+
+#' Title
+#' 
+#' @description
+#'
+#' @param lasttime 
+#' @param holiday 
+#' @param sex 
+#' @param type_of_travel 
+#'
+#' @return
+give_blood <- function(lasttime, holiday, sex, type_of_travel){
+  # Skriv din funktion här
+}
+
+
+
+
+
+# OBS: kommentera bort all kod som inte är de obligatoriska variablerna ovan 
+# eller funktionerna som defineras i inlämningsuppgifterna!
+
+
+# Använda markmyassignment
+library(markmyassignment)
+set_assignment("https://raw.githubusercontent.com/STIMALiU/KursRprgm2/main/Labs/Tests/inl3.yml")
+show_tasks()
+mark_my_assignment()
