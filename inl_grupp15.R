732948e1a57de9d376dc0b230847af8e0424e7da
samuel-sampan <business.samuelolivares@gmail.com>
2026-05-16 15:20:54 +0200

    Refactor inl_grupp15.R and implement give_blood
    Updated comments and added functionality to give_blood function.

--- inl_grupp15.R
+++ inl_grupp15.R
@@ -1,5 +1,8 @@
 #### Programmering i R ####
-#### Datorlaboration  [3] ####
+#### Datorlaboration  [nummer] ####
+
+# spara denna fil enligt:
+# inl3_grupp01.R om ni tillhör grupp 01 och det är inl 3.
 
 
 # Skiv era namn här
@@ -9,6 +12,8 @@ Namn <- c("Mustafa Helali", "Samuel Olivares")
 # Skiv era liu ID här
 LiuId  <- c("mushe150", "samol695")
 
+# På del 2 i kursen, när ni arbetar i par, ange även grupptillhörighet enligt: 
+# Grupp<-"grupp01", om ni tillhör grupp01
 Grupp<-"grupp15"
 
 
@@ -38,12 +43,20 @@ sum_of_dice <- function(N, K){
 
 
 sum_of_random_dice <- function(K, lambda, my_seed=NULL){
+  
+  #kolla om my_seed skiljer sig från null. Om inte väljs random seed.
+  
   if(!is.null(my_seed)) {
     set.seed(my_seed)
   }
+  
+  #skapa matrisen med K rader och 2 kolumner. Kolumners namn är Value och Dice
+  
   x<-matrix(nrow = K, ncol = 2)
   colnames(x) <-c("Value","Dice")
   rownames(x) <-c(1:K)
+  
+  #skapa poissonfördelad matris efter parametrar K och n
   
  for(i in 1:K) {
    n<-rpois(1, lambda = lambda)
@@ -107,11 +120,57 @@ table(z[,1],z[,2])
 #' @param type_of_travel 
 #'
 #' @return
-give_blood <- function(lasttime, holiday, sex, type_of_travel){
-  # Skriv din funktion här
+
+install.packages("lubridate")
+
+library(lubridate)
+
+give_blood <- function(lasttime = today(), holiday = "hemma", sex, type_of_travel = NULL) {
+  
+  lasttime <- as.Date(lasttime)
+  
+  # räkna ut extraTime beroende på resa
+  if (identical(holiday, "hemma")) {
+    extraTime <- lasttime
+  } else {
+    end_date <- as.Date(int_end(holiday))
+    if (type_of_travel == "malaria") {
+      extraTime <- end_date + months(6)
+    } else {
+      extraTime <- end_date + weeks(4)
+    }
+  }
+  
+  # tidigaste möjliga datum baserat på kön
+  if (sex == "f") {
+    suggestion <- lasttime + months(4)
+  } else {
+    suggestion <- lasttime + months(3)
+  }
+  
+  # jämför suggestion med extraTime
+  if (suggestion > extraTime) {
+    proposal <- suggestion
+  } else {
+    proposal <- extraTime + days(1)
+  }
+  
+  # se till att förslaget är en vardag (måndag-fredag)
+  # wday: 1 = söndag, 7 = lördag
+  while (wday(proposal) %in% c(1, 7)) {
+    proposal <- proposal + days(1)
+  }
+  
+  # returnera som textsträng
+  yr <- year(proposal)
+  month_names <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+  mo <- month_names[month(proposal)]
+  dy <- day(proposal)
+  weekday_names <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  wd <- weekday_names[wday(proposal)]
+  
+  return(paste0("year=", yr, " month=", mo, " day=", dy, " weekday=", wd))
 }
-
-
 
 
 
